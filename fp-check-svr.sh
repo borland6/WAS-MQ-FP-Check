bf6520573f67c799025adbb3326be97fda1218fa
Dennis <12634756+borland6@users.noreply.github.com>
2026-05-28 16:36:09 +0800

    modify index.html on 2026/05/28 16:36:05

--- fp-check-svr.sh
+++ fp-check-svr.sh
@@ -1,13 +1,17 @@
+NOW=$(date +'%Y/%m/%d %H:%M:%S')
+COMMIT_MSG="modify index.html on ${NOW}"
 python3 check_fixpack.py
 cp -f fp-check.html index.html
+git add .
+git commit -m "${COMMIT_MSG}"
 # 1. 檢查是否存在名為 ibmfp 的容器（無論運行中或已停止）
-if [ "$(docker ps -aq -f name=^ibmfp$)" ]; then
-    echo "發現舊的容器，正在停止並移除..."
+#if [ "$(docker ps -aq -f name=^ibmfp$)" ]; then
+#    echo "發現舊的容器，正在停止並移除..."
     # 2. 停止並移除容器
-    docker rm -f ibmfp
-else
-    echo "沒有發現舊的容器，直接準備啟動。"
-fi
+#    docker rm -f ibmfp
+#else
+#    echo "沒有發現舊的容器，直接準備啟動。"
+#fi
 
 # 3. 啟動新的容器
-docker run -d --name ibmfp -p 8888:80 -v $(pwd)/index.html:/usr/share/nginx/html/index.html nginx
+#docker run -d --name ibmfp -p 8888:80 -v $(pwd)/index.html:/usr/share/nginx/html/index.html nginx
